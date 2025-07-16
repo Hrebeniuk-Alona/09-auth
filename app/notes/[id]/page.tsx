cd89b09cedccfdf247822dcb3157d509fceb00e3
Alona Hrebeniuk <alena.grebenyuk@ukr.net>
2025-07-16 21:51:15 +0300

    notes,id,page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -11,17 +11,17 @@ type Props = {
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
     const { id } = await params;
     const noteId = Number(id);
-    const note = fetchNoteById(noteId)
+    const note = await fetchNoteById(noteId)
      
     
 
     return {
-        title: (await note).title,
-        description: (await note).content,
+        title: note.title,
+        description: note.content,
         openGraph: {
-            title: `${(await note).title} – Notes | NoteHub`,
-            description: (await note).content,
-            url: `https://notehub-public.goit.study/api/notes/${(await note).tag ?? "All"}`,
+            title: `${note.title} – Notes | NoteHub`,
+            description: note.content,
+            url: `https://notehub-public.goit.study/api/notes/${note.id ?? "All"}`,
             images: [
                 {
                     url: `https://opengraph.b-cdn.net/production/og-image/d96f0dfd-699a-479d-ac06-bdb53631d349.png?expires=33288302765&token=ywDpHK6KmEtpWc8I4n3BiDWO-Br1mL5Md-mnrdW6XqA`,
